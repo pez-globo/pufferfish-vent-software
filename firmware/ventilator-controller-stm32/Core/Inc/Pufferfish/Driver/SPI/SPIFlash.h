fbf8eacba823d07c9cb679315745a75301757716
Geetha Sujana <medavarapu.geetha@avinsystems.com>
2020-08-30 21:31:52 +0530

    Implemented SPI flash memory driver (#19)

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/SPI/SPIFlash.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/SPI/SPIFlash.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include "Pufferfish/HAL/STM32/HALSPIDevice.h"
+#include "Pufferfish/HAL/HAL.h"
 
 namespace Pufferfish {
 namespace Driver {
@@ -19,7 +19,7 @@ namespace SPI {
 class SPIFlash {
 
 public:
-  SPIFlash (HAL::HALSPIDevice &spi):mSpi(spi){
+  SPIFlash (HAL::SPIDevice &spi):mSpi(spi){
   }
 
   /** Reads the spi device id
@@ -50,7 +50,7 @@ public:
      * @param size amount of data to be receive
      * @return ok on success, error code otherwise
      */
-   SPIDeviceStatus readData(uint32_t addr, uint8_t &rxBuf, uint8_t size);
+   SPIDeviceStatus readData(uint32_t addr, uint8_t *rxBuf, uint8_t size);
 
    /** Chip Erase
      *
@@ -69,42 +69,80 @@ public:
    /** Block Erase - 32KB
      *
      * @param addr address to erase the block
-     * @return true on success, false otherwise
+     * @return ok on success, writeError or blockProtect otherwise
      */
-   bool eraseBlock32KB(uint32_t addr);
+   SPIDeviceStatus eraseBlock32KB(uint32_t addr);
 
    /** Block Erase - 64KB
      *
      * @param addr address to erase the block
-     * @return true on success, false otherwise
+     * @return ok on success, writeError or blockProtect otherwise
      */
-   bool eraseBlock64KB(uint32_t addr);
-
-   /** Read byte from the spi device
-     *
-     * @param addr address to read data
-     * @param rxBuf pointer to reception data buffer
-     * @return ok on success, error code otherwise
-     */
-   SPIDeviceStatus readByte(uint32_t addr, uint8_t &rxBuf);
+   SPIDeviceStatus eraseBlock64KB(uint32_t addr);
 
    /** write byte into the spi device
      *
      * @param addr address to write data
      * @param input data to be written
+     * @return notBusy on success, busy otherwise
      */
-   void writeByte(uint32_t addr, uint8_t input);
+   SPIDeviceStatus writeByte(uint32_t addr, uint8_t input);
+
+   /** read status from the spi device
+     *
+     * @param void
+     * @param input data to be written
+     * @return notBusy on success, busy otherwise
+     */
+   SPIDeviceStatus readSpiStatus(void);
+
+   /** protect block for the spi device
+     *
+     * @param void
+     * @param input data to be written
+     * @return blockProtect on success, busy otherwise
+     */
+   SPIDeviceStatus protectBlock(void);
+
+   /** read block protect status from the spi device
+     *
+     * @param void
+     * @param input data to be written
+     * @return blockProtect on success, blockNotProtect otherwise
+     */
+   SPIDeviceStatus readBlockProtectStatus(void);
+
+   /** lock the block based on address
+     *
+     * @param addr address of the lock block
+     * @return lockBlock on success, lockUnBlock otherwise
+     */
+   SPIDeviceStatus lockBlock(uint32_t addr);
+
+   /** unlock the block based on address
+     *
+     * @param addr address of the unlock block
+     * @return lockUnBlock on success, lockBlock otherwise
+     */
+   SPIDeviceStatus unLockBlock(uint32_t addr);
 
 private:
-   HAL::HALSPIDevice &mSpi;
+   HAL::SPIDevice &mSpi;
    static const uint8_t deviceIdInstruction =0x90;
    static const uint8_t readDataInstruction =0x03;
+   static const uint8_t writeDataInstruction =0x02;
    static const uint8_t writeEnableInstruction = 0x06;
    static const uint8_t writeDisableInstruction = 0x04;
    static const uint8_t chipEraseInstruction = 0x60;
    static const uint8_t sectorErase4KBInstruction = 0x20;
    static const uint8_t blockErase32KBInstruction = 0x52;
    static const uint8_t blockErase64KBInstruction = 0xD8;
+   static const uint8_t readStatusReg1 =0x05;
+   static const uint8_t writeStatusReg3 =0x11;
+   static const uint8_t readStatusReg3 =0x15;
+   static const uint8_t writeProtectSelection =0x04;
+   static const uint8_t lockBlockInstruction =0x36;
+   static const uint8_t unlockBlockInstruction =0x39;
 };
 
 }  // namespace SPI
--- firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/SPI/SPIFlash.cpp
+++ firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/SPI/SPIFlash.cpp
@@ -42,126 +42,268 @@ SPIDeviceStatus SPIFlash::disableWrite(void){
   SPIDeviceStatus stat = mSpi.write(&temp, 1);
 
   if (stat == SPIDeviceStatus::ok) {
-      return SPIDeviceStatus::ok;
-    } else {
-      return SPIDeviceStatus::writeError;
-    }
+    return SPIDeviceStatus::ok;
+  } else {
+    return SPIDeviceStatus::writeError;
+  }
 
 }
 
-SPIDeviceStatus SPIFlash::readData(uint32_t addr, uint8_t &rxBuf, uint8_t size){
+SPIDeviceStatus SPIFlash::readData(uint32_t addr, uint8_t *rxBuf, uint8_t size){
   uint8_t txbuf[size+4] = {0};
 
   txbuf[0]= readDataInstruction;
-  for (uint8_t index = 1; index<=3 ; index++)
-  {
+  for (uint8_t index = 1; index<=3 ; index++){
     txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
   }
 
-  SPIDeviceStatus stat = mSpi.writeRead(txbuf, &rxBuf, size+4);
+  SPIDeviceStatus stat = mSpi.writeRead(txbuf, rxBuf, size+4);
   if (stat == SPIDeviceStatus::ok) {
-        return SPIDeviceStatus::ok;
-      } else {
-        return SPIDeviceStatus::readError;
-      }
+    return SPIDeviceStatus::ok;
+  } else {
+    return SPIDeviceStatus::readError;
+  }
+
 }
 
 bool SPIFlash::eraseChip(void){
   uint8_t temp=writeDisableInstruction;
+
+  this->enableWrite();
   SPIDeviceStatus stat = mSpi.write(&temp, 1);
+  this->disableWrite();
   if (stat == SPIDeviceStatus::ok) {
     return true;
   } else {
     return false;
   }
+
 }
 
 bool SPIFlash::eraseSector4KB(uint32_t addr){
   uint8_t txbuf[4] = {0};
   txbuf[0]= sectorErase4KBInstruction;
 
-  for (uint8_t index = 1; index<=3 ; index++)
-  {
+  this->enableWrite();
+  for (uint8_t index = 1; index<=3 ; index++){
     txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
   }
 
   SPIDeviceStatus stat = mSpi.write(txbuf, 4);
+  this->disableWrite();
   if (stat == SPIDeviceStatus::ok) {
     return true;
   } else {
     return false;
   }
+
 }
 
-bool SPIFlash::eraseBlock32KB(uint32_t addr){
+SPIDeviceStatus SPIFlash::eraseBlock32KB(uint32_t addr){
   uint8_t txbuf[4] = {0};
   txbuf[0]= blockErase32KBInstruction;
-  for (uint8_t index = 1; index<=3 ; index++){
-      txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
-    }
 
-  SPIDeviceStatus stat = mSpi.write(txbuf, 4);
-  if (stat == SPIDeviceStatus::ok) {
-    return true;
+  SPIDeviceStatus protectStatus = this->readBlockProtectStatus();
+  SPIDeviceStatus blockStatus = this->unLockBlock(addr);
+  if(protectStatus == SPIDeviceStatus::blockNotProtect
+      && blockStatus == SPIDeviceStatus::blockUnLock ){
+       this->enableWrite();
+       for (uint8_t index = 1; index<=3 ; index++){
+         txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
+       }
+
+       SPIDeviceStatus stat = mSpi.write(txbuf, 4);
+       this->disableWrite();
+       if (stat == SPIDeviceStatus::ok) {
+         return SPIDeviceStatus::ok;
+       } else {
+         return SPIDeviceStatus::writeError;
+       }
   } else {
-    return false;
+    return SPIDeviceStatus::blockProtect;
   }
 
 }
 
-bool SPIFlash::eraseBlock64KB(uint32_t addr){
+SPIDeviceStatus SPIFlash::eraseBlock64KB(uint32_t addr){
   uint8_t txbuf[4] = {0};
   txbuf[0]= blockErase64KBInstruction;
-  for (uint8_t index = 1; index<=3 ; index++){
-      txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
-    }
 
-  SPIDeviceStatus stat = mSpi.write(txbuf, 4);
-  if (stat == SPIDeviceStatus::ok) {
-    return true;
+  SPIDeviceStatus protectStatus = this->readBlockProtectStatus();
+  SPIDeviceStatus blockStatus = this->unLockBlock(addr);
+  if(protectStatus == SPIDeviceStatus::blockNotProtect
+      && blockStatus == SPIDeviceStatus::blockUnLock ){
+       this->enableWrite();
+       for (uint8_t index = 1; index<=3 ; index++){
+           txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
+       }
+
+       SPIDeviceStatus stat = mSpi.write(txbuf, 4);
+       this->disableWrite();
+       if (stat == SPIDeviceStatus::ok) {
+         return SPIDeviceStatus::ok;
+       } else {
+         return SPIDeviceStatus::writeError;
+       }
+ } else {
+   return SPIDeviceStatus::blockProtect;
+ }
+
+}
+
+SPIDeviceStatus SPIFlash::writeByte(uint32_t addr, uint8_t input){
+  static uint8_t  defaultsize = 1;
+  uint8_t byteAddr = 0;
+  uint8_t temp= writeDataInstruction;
+
+  SPIDeviceStatus status = this->readSpiStatus();
+  if(status == SPIDeviceStatus::notbusy){
+
+    this->enableWrite();
+    mSpi.write(&temp, defaultsize);
+
+    byteAddr = static_cast<uint8_t>(((addr&0xFF000000) >> 24) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    byteAddr = static_cast<uint8_t>(((addr&0x00FF00000) >> 16) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    byteAddr = static_cast<uint8_t>(((addr&0x0000FF00) >> 8) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    byteAddr = static_cast<uint8_t>((addr&0x000000FF) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    mSpi.write(&input, defaultsize);
+    this->disableWrite();
+
+    return status;
   } else {
-    return false;
+    return status;
   }
 
 }
 
-SPIDeviceStatus SPIFlash::readByte(uint32_t addr, uint8_t &rxBuf){
-  uint8_t txbuf[4] = {0};
-  for (uint8_t index = 0; index<=3 ; index++)
-    {
-      txbuf[index] = (addr >> (8 * (3-index))) & 0xFF;
+SPIDeviceStatus SPIFlash::readSpiStatus(void){
+  uint8_t txbuf[1] = {0};
+  uint8_t rxbuf[1] = {0};
+  uint8_t  defaultsize = 1;
+
+  txbuf[0]= readStatusReg1;
+  mSpi.writeRead(txbuf, rxbuf, defaultsize);
+  if(((*rxbuf) & 0x01) == 1){
+    return SPIDeviceStatus::busy;
+  } else {
+    return SPIDeviceStatus::notbusy;
+  }
+
+}
+
+SPIDeviceStatus SPIFlash::protectBlock(void){
+  uint8_t txbuf[2]= {writeStatusReg3,writeProtectSelection};
+  uint8_t defaultsize = 2;
+
+  SPIDeviceStatus ret = this->readSpiStatus();
+  if(ret == SPIDeviceStatus::notbusy){
+    this->enableWrite();
+    mSpi.write(txbuf, defaultsize);
+    this->disableWrite();
+
+    return SPIDeviceStatus::blockProtect;
+  } else {
+    return ret;
+  }
+
+}
+
+SPIDeviceStatus SPIFlash::readBlockProtectStatus(void){
+  uint8_t txbuf[1] = {0};
+  uint8_t rxbuf[1] = {0};
+  uint8_t  defaultsize = 1;
+
+  txbuf[0]= readStatusReg3;
+  mSpi.writeRead(txbuf, rxbuf, defaultsize);
+  if(((*rxbuf) & 0x04) == 1){
+    return SPIDeviceStatus::blockProtect;
+  } else {
+    return SPIDeviceStatus::blockNotProtect;
+  }
+
+}
+
+SPIDeviceStatus SPIFlash::lockBlock(uint32_t addr){
+  uint8_t byteAddr =0;
+  uint8_t input =lockBlockInstruction;
+  uint8_t defaultsize =1;
+  SPIDeviceStatus ret;
+
+  SPIDeviceStatus protectStatus = this->readBlockProtectStatus();
+  if(protectStatus == SPIDeviceStatus::blockNotProtect){
+    this->protectBlock();
+  }
+
+  SPIDeviceStatus status = this->readSpiStatus();
+  if(status == SPIDeviceStatus::notbusy){
+      this->enableWrite();
+      mSpi.write(&input, defaultsize);
+
+      byteAddr = static_cast<uint8_t>(((addr&0xFF000000) >> 24) & 0xFF);
+      mSpi.write(&byteAddr, defaultsize);
+
+      byteAddr = static_cast<uint8_t>(((addr&0x00FF00000) >> 16) & 0xFF);
+      mSpi.write(&byteAddr, defaultsize);
+
+      byteAddr = static_cast<uint8_t>(((addr&0x0000FF00) >> 8) & 0xFF);
+      mSpi.write(&byteAddr, defaultsize);
+
+      byteAddr = static_cast<uint8_t>((addr&0x000000FF) & 0xFF);
+      mSpi.write(&byteAddr, defaultsize);
+
+      this->disableWrite();
+      ret = SPIDeviceStatus::blockLock;
+    } else {
+      ret = SPIDeviceStatus::busy;
     }
-
-    SPIDeviceStatus stat = mSpi.writeRead(txbuf, &rxBuf, 1);
-    if (stat == SPIDeviceStatus::ok) {
-          return SPIDeviceStatus::ok;
-        } else {
-          return SPIDeviceStatus::readError;
-        }
-
+    return ret;
 }
 
-// FIXME: we can do by using union
-void SPIFlash::writeByte(uint32_t addr, uint8_t input){
-  static uint8_t  defaultsize = 1;
-  uint8_t byteAddr = 0;
-  this->enableWrite();
-  byteAddr = static_cast<uint8_t>(((addr&0xFF000000) >> 24) & 0xFF);
-  mSpi.write(&byteAddr, defaultsize);
 
-  byteAddr = static_cast<uint8_t>(((addr&0x00FF00000) >> 16) & 0xFF);
-  mSpi.write(&byteAddr, defaultsize);
+SPIDeviceStatus SPIFlash::unLockBlock(uint32_t addr){
+  uint8_t byteAddr =0;
+  uint8_t input =unlockBlockInstruction;
+  uint8_t defaultsize =1;
+  SPIDeviceStatus ret;
 
-  byteAddr = static_cast<uint8_t>(((addr&0x0000FF00) >> 8) & 0xFF);
-  mSpi.write(&byteAddr, defaultsize);
+  SPIDeviceStatus protectStatus = this->readBlockProtectStatus();
+  if(protectStatus == SPIDeviceStatus::blockNotProtect){
+    this->protectBlock();
+  }
 
-  byteAddr = static_cast<uint8_t>((addr&0x000000FF) & 0xFF);
-  mSpi.write(&byteAddr, defaultsize);
+  SPIDeviceStatus status = this->readSpiStatus();
+  if(status == SPIDeviceStatus::notbusy){
+    this->enableWrite();
+    mSpi.write(&input, defaultsize);
 
-  mSpi.write(&input, defaultsize);
-  this->disableWrite();
+    byteAddr = static_cast<uint8_t>(((addr&0xFF000000) >> 24) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
 
+    byteAddr = static_cast<uint8_t>(((addr&0x00FF00000) >> 16) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    byteAddr = static_cast<uint8_t>(((addr&0x0000FF00) >> 8) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    byteAddr = static_cast<uint8_t>((addr&0x000000FF) & 0xFF);
+    mSpi.write(&byteAddr, defaultsize);
+
+    this->disableWrite();
+    ret = SPIDeviceStatus::blockUnLock;
+  }else {
+    ret = SPIDeviceStatus::busy;
+  }
+    return ret;
 }
+
 
 }  // namespace SPI
 }  // namespace Driver
