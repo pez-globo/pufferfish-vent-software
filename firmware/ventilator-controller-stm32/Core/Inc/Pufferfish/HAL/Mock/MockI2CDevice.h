64d753b5248593ac94f5d8e83091c08bc575e878
hemanthgs <hemanth.gowda@avinsystems.com>
2020-09-02 14:29:36 +0530

    Upadted comments for MockI2CDevice.h

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/Mock/MockI2CDevice.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/Mock/MockI2CDevice.h
@@ -26,32 +26,32 @@ class MockI2CDevice : public I2CDevice {
 
   /**
    * @brief  Read method to read data from private buffer variable mReadBuf
-   * @param  buf -
-   * @param  count =
+   * @param  buf returns the data stored in setRead method
+   * @param  count size of data to set
    * @return returns I2CDeviceStatus::ok
    */
   I2CDeviceStatus read(uint8_t *buf, size_t count) override;
 
   /**
    * @brief  Updates the private buffer variable mReadBuf with the input data
-   * @param  buf
-   * @param  count
+   * @param  buf update the private variable mReadBuf with the buffer input
+   * @param  count size of data to set
    * @return None
    */
   void setRead(uint8_t *buf, size_t count);
 
   /**
    * @brief  Updates the private buffer variable mWriteBuf with the input data
-   * @param  buf
-   * @param  count
+   * @param  buf update the private variable mWriteBuf with the buffer input
+   * @param  count size of data to write
    * @return returns I2CDeviceStatus::ok
    */
   I2CDeviceStatus write(uint8_t *buf, size_t count) override;
 
   /**
    * @brief  Reads private buffer variable mWriteBuf and updates to buf
-   * @param  buf
-   * @param  count
+   * @param  buf returns the data stored in write method
+   * @param  count buffer size to return
    * @return None
    */
   void getWrite(uint8_t *buf, size_t count);
