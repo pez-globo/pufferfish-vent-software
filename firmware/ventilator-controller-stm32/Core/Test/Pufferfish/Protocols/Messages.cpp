0c50c31faadb2cf7cc7f9b7b1b850022ee1c3da2
Renji Panicker <renjipanicker@gmail.com>
2020-11-18 17:40:49 +0530

    WIP: adding State tests

--- firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/Messages.cpp
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/Messages.cpp
@@ -12,6 +12,7 @@
 
 #include "Pufferfish/Protocols/Messages.h"
 #include "Pufferfish/Application/States.h"
+#include "Pufferfish/BackendDefs.h"
 #include "Pufferfish/Protocols/Chunks.h"
 #include "Pufferfish/Util/Vector.h"
 #include "Pufferfish/Util/Array.h"
@@ -20,19 +21,8 @@
 #include "catch2/catch.hpp"
 
 namespace PF = Pufferfish;
+namespace BE = PF::Driver::Serial::Backend;
 
-static const auto message_descriptors =
-PF::Util::make_array<PF::Util::ProtobufDescriptor>(
-    // array index should match the type code value
-    PF::Util::get_protobuf_descriptor<PF::Util::UnrecognizedMessage>(),  // 0
-    PF::Util::get_protobuf_descriptor<Alarms>(),                         // 1
-    PF::Util::get_protobuf_descriptor<SensorMeasurements>(),             // 2
-    PF::Util::get_protobuf_descriptor<CycleMeasurements>(),              // 3
-    PF::Util::get_protobuf_descriptor<Parameters>(),                     // 4
-    PF::Util::get_protobuf_descriptor<ParametersRequest>(),              // 5
-    PF::Util::get_protobuf_descriptor<Ping>(),                           // 6
-    PF::Util::get_protobuf_descriptor<Announcement>()                    // 7
-);
 static constexpr size_t num_descriptors = 8;
 
 SCENARIO("Protocols::Message behaves correctly", "[messages]") {
@@ -48,7 +38,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       constexpr size_t buffer_size = 252UL;
       test_message.payload.tag = PF::Application::MessageTypes::alarms;
       PF::Util::ByteVector<buffer_size> buffer;
-      auto status = test_message.write(buffer, message_descriptors);
+      auto status = test_message.write(buffer, BE::message_descriptors);
 
       THEN("the final status should be ok") {
         REQUIRE(status == PF::Protocols::MessageStatus::ok);
@@ -59,7 +49,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       constexpr size_t buffer_size = 252UL;
       test_message.payload.tag = PF::Application::MessageTypes::unknown;
       PF::Util::ByteVector<buffer_size> buffer;
-      auto status = test_message.write(buffer, message_descriptors);
+      auto status = test_message.write(buffer, BE::message_descriptors);
 
       THEN("the final status should be invalid_type") {
         REQUIRE(status == PF::Protocols::MessageStatus::invalid_type);
@@ -70,7 +60,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       constexpr size_t buffer_size = 0UL;
       test_message.payload.tag = PF::Application::MessageTypes::alarms;
       PF::Util::ByteVector<buffer_size> buffer;
-      auto status = test_message.write(buffer, message_descriptors);
+      auto status = test_message.write(buffer, BE::message_descriptors);
 
       THEN("the final status should be invalid_length") {
         REQUIRE(status == PF::Protocols::MessageStatus::invalid_length);
@@ -81,9 +71,9 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       constexpr size_t buffer_size = 253UL;
       test_message.payload.tag = PF::Application::MessageTypes::alarms;
       PF::Util::ByteVector<buffer_size> buffer;
-      auto write_status = test_message.write(buffer, message_descriptors);
+      auto write_status = test_message.write(buffer, BE::message_descriptors);
       buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarms);
-      auto parse_status = test_message.parse(buffer, message_descriptors);
+      auto parse_status = test_message.parse(buffer, BE::message_descriptors);
 
       THEN("the final status should be ok") {
         REQUIRE(write_status == PF::Protocols::MessageStatus::ok);
@@ -97,7 +87,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       PF::Util::ByteVector<buffer_size> buffer;
       buffer.resize(buffer_size);
       buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarms);
-      auto status = test_message.parse(buffer, message_descriptors);
+      auto status = test_message.parse(buffer, BE::message_descriptors);
 
       THEN("the final status should be ok") {
         REQUIRE(status == PF::Protocols::MessageStatus::invalid_encoding);
@@ -106,7 +96,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
 
     WHEN("the regular alarm data is parsed from the buffer") {
       constexpr size_t buffer_size = 253UL;
-      PF::Protocols::MessageSender<TestMessage, num_descriptors> sender{message_descriptors};
+      PF::Protocols::MessageSender<TestMessage, num_descriptors> sender{BE::message_descriptors};
       test_message.payload.tag = PF::Application::MessageTypes::alarms;
       PF::Util::ByteVector<buffer_size> buffer;
       auto transform_status = sender.transform(test_message, buffer);
@@ -118,11 +108,11 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
 
     WHEN("the regular alarm data is written into the buffer") {
       constexpr size_t buffer_size = 253UL;
-      PF::Protocols::MessageReceiver<TestMessage, num_descriptors> receiver{message_descriptors};
+      PF::Protocols::MessageReceiver<TestMessage, num_descriptors> receiver{BE::message_descriptors};
       test_message.payload.tag = PF::Application::MessageTypes::alarms;
 
       PF::Util::ByteVector<buffer_size> buffer;
-      auto write_status = test_message.write(buffer, message_descriptors);
+      auto write_status = test_message.write(buffer, BE::message_descriptors);
       buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarms);
 
       auto transform_status = receiver.transform(buffer, test_message);
--- firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/States.cpp
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/States.cpp
@@ -11,10 +11,12 @@
  */
 
 #include "Pufferfish/Protocols/States.h"
+#include "Pufferfish/Protocols/Chunks.h"
 #include "Pufferfish/Application/States.h"
-#include "Pufferfish/Driver/Serial/Backend/Backend.h"
+#include "Pufferfish/BackendDefs.h"
 #include "Pufferfish/Util/Array.h"
 #include "Pufferfish/HAL/Mock/MockTime.h"
+#include "Pufferfish/HAL/CRCChecker.h"
 
 #include "catch2/catch.hpp"
 
@@ -31,14 +33,36 @@ SCENARIO("Protocols::State behaves correctly", "[Datagram]") {
     PF::Application::States states;
     BackendStateSynchronizer synchronizer{states, PF::Driver::Serial::Backend::state_sync_schedule};
 
+    WHEN("the current time is written to it") {
+      PF::HAL::MockTime time;
+      const uint32_t ctime = 7867643;
+      time.set_millis(ctime);
+      auto current_time = time.millis();
+      THEN("the final status should be ok") {
+        REQUIRE(current_time == ctime);
+      }
+    }
+
     WHEN("data is written to it") {
-      // PF::HAL::MockTime time;
-      // const uint32_t ctime = 7867643;
-      // time.set_millis(ctime);
-      // auto current_time = time.millis();
-      // THEN("the final status should be ok") {
-      //   REQUIRE(current_time == ctime);
-      // }
+      PF::HAL::SoftCRC32 crc32c{PF::HAL::crc32c_params};
+      constexpr size_t buffer_size = 256;
+      PF::Protocols::ChunkSplitter<buffer_size, uint8_t> chunks;
+      uint8_t val = 128;
+      PF::Protocols::ChunkInputStatus status;
+      for(uint8_t i = 1; i < 100; ++i) {
+          status = chunks.input(val);
+      }
+
+    //   // BackendMessage message;
+    //   // auto output_status = receiver.output(message);
+
+    //   PF::HAL::MockTime time;
+    //   const uint32_t ctime = 7867643;
+    //   time.set_millis(ctime);
+    //   auto current_time = time.millis();
+    //   THEN("the final status should be ok") {
+    //     REQUIRE(current_time == ctime);
+    //   }
     }
 
   }
