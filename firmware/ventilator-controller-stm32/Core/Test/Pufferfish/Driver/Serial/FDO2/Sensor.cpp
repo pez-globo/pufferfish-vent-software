10a190cd858c2d48c1dc39b97008c963a4ff8099
rohan <purohit.rohan@gmail.com>
2020-11-26 18:05:48 +0530

    Add test cases for FDO2 driver

---
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Driver/Serial/FDO2/Sensor.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020, the Pez Globo team and the Pufferfish project contributors
+ *
+ * Device.cpp
+ *
+ *  Created on: Nov 25, 2020
+ *      Author: Rohan Purohit
+ *
+ * Unit tests to confirm behavior of flow sensor driver
+ *
+ */
+#include "Pufferfish/Driver/Serial/FDO2/Sensor.h"
+#include "Pufferfish/HAL/Mock/MockBufferedUART.h"
+#include "Pufferfish/HAL/Mock/MockTime.h"
+
+#include "catch2/catch.hpp"
+
+namespace PF = Pufferfish;
+
+SCENARIO("FDO2: flow sensor behaves properly", "[sensor]") {
+    GIVEN("A Mock I2C device") {
+        constexpr size_t rx_buffer_size = 252UL;
+        constexpr size_t tx_buffer_size = 252UL;
+
+        PF::HAL::MockBufferedUART<rx_buffer_size, tx_buffer_size> uart{};
+        auto body = std::string("\x04\x02\x06\x11");
+        uart.write(body.c_str());
+
+        PF::Driver::Serial::FDO2::Device device{uart};
+
+        WHEN("the device is initialised") {
+            const uint32_t ctime = 0x3200;
+            PF::HAL::MockTime time;
+            time.set_millis(ctime);
+
+            auto current_time = time.millis();
+
+            PF::Driver::Serial::FDO2::Sensor sensor(device, time);
+
+            auto status = sensor.setup();
+
+            THEN("the final time should be the same") {
+                REQUIRE(current_time == ctime);
+                REQUIRE(status == PF::InitializableState::ok);
+                // will fail as there is no return for request_version command in Mock UART device
+            }
+        }
+    }
+
+    GIVEN("A Mock I2C device") {
+        constexpr size_t rx_buffer_size = 252UL;
+        constexpr size_t tx_buffer_size = 252UL;
+
+        PF::HAL::MockBufferedUART<rx_buffer_size, tx_buffer_size> uart{};
+        auto body = std::string("\x04\x02\x06\x11");
+        uart.write(body.c_str());
+        PF::HAL::MockTime time;
+        PF::Driver::Serial::FDO2::Device device{uart};
+
+        WHEN("the device is initialised") {
+            const uint32_t ctime = 0x5000;
+            time.set_millis(ctime);
+
+            PF::Driver::Serial::FDO2::Sensor sensor(device, time);
+
+            auto setup_status = sensor.setup();
+
+            uint32_t po2 = 20;
+            auto output_status = sensor.output(po2);
+            
+            THEN("the final status should ok") {
+                REQUIRE(output_status == PF::InitializableState::ok);
+                // Will fail as there is no return for check version in Mock Uart device
+
+            }
+        }
+    }
+}
