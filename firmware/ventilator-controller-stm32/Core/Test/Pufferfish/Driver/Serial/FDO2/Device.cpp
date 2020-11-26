dc4e80753627f70b173d24b9390273617a8edc8e
rohan <purohit.rohan@gmail.com>
2020-11-26 18:05:48 +0530

    Driver: Add unit test cases for FDO2 driver

---
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Driver/Serial/FDO2/Device.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2020, the Pez Globo team and the Pufferfish project contributors
+ *
+ * Device.cpp
+ *
+ *  Created on: Nov 26, 2020
+ *      Author: Rohan Purohit
+ *
+ * Unit tests to confirm behavior of flow sensor driver
+ *
+ */
+#include "Pufferfish/Driver/Serial/FDO2/Device.h"
+#include "Pufferfish/HAL/Mock/MockBufferedUART.h"
+
+#include "catch2/catch.hpp"
+
+namespace PF = Pufferfish;
+
+SCENARIO("FDO2: SFM3000 flow meter driver behaves properly", "[device]") {
+
+    GIVEN("A mock Uart device") {
+        constexpr size_t rx_buffer_size = 252UL;
+        constexpr size_t tx_buffer_size = 252UL;
+
+        PF::HAL::MockBufferedUART<rx_buffer_size, tx_buffer_size> uart{};
+        auto body = std::string("\x04\x02\x06\x11");
+        uart.write(body.c_str());
+
+        PF::Driver::Serial::FDO2::Device device{uart};
+
+        WHEN("the device starts broadcast") {
+            auto status = device.start_broadcast();
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::Device::Status::ok);                
+            }
+        }
+
+        WHEN("the device receives response") {
+            PF::Driver::Serial::FDO2::Responses::Vers vers{};
+            vers.type = 1;
+            vers.device_id = 2;
+            vers.num_channels = 3;
+            vers.firmware_rev = 123456;
+
+            PF::Driver::Serial::FDO2::Response response;
+            response.tag = PF::Driver::Serial::FDO2::CommandTypes::vers;
+            response.value.vers = vers;
+
+            auto status = device.receive(response);
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::Device::Status::ok);                
+            }
+        }
+
+        WHEN("the device flashes led") {
+            auto status = device.flash_led();
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::Device::Status::ok);                
+            }
+        }
+
+        WHEN("the device requests version") {
+            auto status = device.request_version();
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::Device::Status::ok);
+                // Fails as the mock uart device should return the firmware version
+            }
+        }
+    }
+
+    GIVEN("A Response receiver") {
+        constexpr size_t buffer_size = 252UL;
+
+        PF::Driver::Serial::FDO2::ResponseReceiver response_receiver{};
+        auto body = std::string("\x04\x02\x06\x11");
+
+        WHEN("a response is received") {
+            auto status = response_receiver.input(body.c_str());
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::ResponseReceiver::InputStatus::ok);
+            }
+        }
+
+        WHEN("an output is sent") {
+            auto input_status = response_receiver.input(body.c_str());
+
+            PF::Driver::Serial::FDO2::Responses::Vers vers{};
+            vers.type = 1;
+            vers.device_id = 2;
+            vers.num_channels = 3;
+            vers.firmware_rev = 123456;
+
+            PF::Driver::Serial::FDO2::Response response;
+            response.tag = PF::Driver::Serial::FDO2::CommandTypes::vers;
+            response.value.vers = vers;
+
+            auto output_status = response_receiver.output(response);
+
+            THEN("status should be ok") {
+                REQUIRE(output_status == PF::Driver::Serial::FDO2::ResponseReceiver::OutputStatus::available);
+                // will fail because of improper input data to Mock UART
+            }
+        }
+    }
+
+    GIVEN("A Request sender") {
+        constexpr size_t buffer_size = 30UL;
+
+        PF::Util::Vector<char, buffer_size> output_buffer;
+        PF::Driver::Serial::FDO2::RequestSender request_sender{};
+
+        PF::Driver::Serial::FDO2::Request request;
+        request.tag = PF::Driver::Serial::FDO2::CommandTypes::vers;
+
+        WHEN("a request is sent") {
+            auto status = request_sender.transform(request, output_buffer);
+            THEN("status should be ok") {
+                REQUIRE(status == PF::Driver::Serial::FDO2::RequestSender::Status::ok);
+            }
+        }
+    }
+}
