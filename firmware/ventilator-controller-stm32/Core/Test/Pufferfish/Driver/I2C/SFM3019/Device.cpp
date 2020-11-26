1a293f644fe52c0bc1ab1dc2672787393b11e539
rohan <purohit.rohan@gmail.com>
2020-11-26 13:36:51 +0530

    Driver: Add unit test cases for SFM3019 driver

---
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Driver/I2C/SFM3019/Device.cpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2020, the Pez Globo team and the Pufferfish project contributors
+ *
+ * Device.cpp
+ *
+ *  Created on: Nov 25, 2020
+ *      Author: Rohan Purohit
+ *
+ * Unit tests to confirm behavior of flow sensor driver
+ *
+ */
+#include "Pufferfish/Driver/I2C/SFM3019/Device.h"
+#include "Pufferfish/HAL/Mock/MockI2CDevice.h"
+#include "Pufferfish/Test/Util.h"
+
+#include "catch2/catch.hpp"
+
+namespace PF = Pufferfish;
+
+SCENARIO("SFM3019: flow sensor driver behaves properly", "[device]") {
+    PF::HAL::MockI2CDevice dev;
+    PF::Driver::I2C::SFM3019::GasType gas = PF::Driver::I2C::SFM3019::GasType::air;
+
+    auto body = std::string("\x04\x02\x06\x11");
+    dev.write(body.c_str(), body.size());
+
+    GIVEN("A Mock I2C device") {
+        PF::Driver::I2C::SFM3019::Device device{dev, dev, gas};
+
+        WHEN("starts a flow measurment") {
+            auto status = device.start_measure();
+            THEN("should return ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("reading product id") {
+            uint32_t product_id = 0x04020611;
+
+            auto status = device.read_product_id(product_id);
+
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("configuring an average window") {
+            uint8_t average_value = 50;
+            auto status = device.set_averaging(average_value);
+            THEN("should return ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("requesting conversion factors") {
+            auto status = device.request_conversion_factors();
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("reading conversion factors after 20 us") {
+            PF::Driver::I2C::SFM3019::ConversionFactors conversion_factors;
+            conversion_factors.flow_unit = 10;
+            auto status = device.read_conversion_factors(conversion_factors);
+
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("A sample is given") {
+            PF::Driver::I2C::SFM3019::Sample sample;
+            PF::Driver::I2C::SFM3019::ConversionFactors conversion_factors;
+            sample.raw_flow = 10;
+            sample.flow = 2.5;
+            conversion_factors.scale_factor = 10;
+            conversion_factors.offset = 5;
+
+            auto status = device.read_sample(sample, conversion_factors.scale_factor, conversion_factors.offset);
+
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+
+        WHEN("stops a flow measurment") {
+            auto status = device.stop_measure();
+
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+    }
+
+    GIVEN("A Mock I2C device") {
+        PF::Driver::I2C::SFM3019::Device device{dev, dev, gas};
+
+        WHEN("reset command is sent") {
+            auto status = device.reset();
+
+            THEN("status should be ok") {
+                REQUIRE(status == PF::I2CDeviceStatus::ok);
+            }
+        }
+    }
+
+}
