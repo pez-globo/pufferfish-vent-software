32197c1e778d0642114e214cd471d4560b358199
rohan <purohit.rohan@gmail.com>
2020-12-28 20:51:07 +0530

    Protocols: Refactor cases
    - after 917ac6302ce011d83b881628b6083f6b4d406e88

--- firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/Messages.cpp
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/Messages.cpp
@@ -17,13 +17,14 @@
 #include "Pufferfish/Util/Vector.h"
 #include "Pufferfish/Util/Array.h"
 #include "nanopb/pb.h"
+#include "Pufferfish/Application/mcu_pb.h"
 
 #include "catch2/catch.hpp"
 
 namespace PF = Pufferfish;
 namespace BE = PF::Driver::Serial::Backend;
 
-static constexpr size_t num_descriptors = 8;
+static constexpr size_t num_descriptors = 7;
 
 SCENARIO("Protocols::Message behaves correctly", "[messages]") {
   GIVEN("A protocol-buffer message") {
@@ -34,14 +35,17 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
     >;
     TestMessage test_message;
 
-    WHEN("the regular alarm data is written to the buffer") {
+    WHEN("the regular parameters request data is written to the buffer") {
       constexpr size_t buffer_size = 252UL;
       PF::Util::ByteVector<buffer_size> buffer;
 
-      Alarms alarms;
-      alarms.alarm_one = true;
-      test_message.payload.set(alarms);
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;     
+      ParametersRequest parameters_request;
+      parameters_request.ventilating=true;
+      parameters_request.fio2=40;
+      parameters_request.mode=VentilationMode_hfnc;
+      parameters_request.flow=60;
+      test_message.payload.set(parameters_request);
+      test_message.payload.tag= PF::Application::MessageTypes::parameters_request;
 
       auto status = test_message.write(buffer, BE::message_descriptors);
 
@@ -61,9 +65,9 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       }
     }
 
-    WHEN("the regular alarm data is written to a buffer of zero size") {
+    WHEN("the regular alarm limits data is written to a buffer of zero size") {
       constexpr size_t buffer_size = 0UL;
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+      test_message.payload.tag = PF::Application::MessageTypes::alarm_limits;
       PF::Util::ByteVector<buffer_size> buffer;
       auto status = test_message.write(buffer, BE::message_descriptors);
 
@@ -72,16 +76,20 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       }
     }
 
-    WHEN("the regular alarm data is parsed from the buffer") {
+    WHEN("the regular parameters request data is parsed from the buffer") {
       constexpr size_t buffer_size = 253UL;
-      Alarms alarms;
-      alarms.alarm_one = true;
-      test_message.payload.set(alarms);
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+
+      ParametersRequest parameters_request;
+      parameters_request.ventilating=true;
+      parameters_request.fio2=40;
+      parameters_request.mode=VentilationMode_hfnc;
+      parameters_request.flow=60;
+      test_message.payload.set(parameters_request);
+      test_message.payload.tag= PF::Application::MessageTypes::parameters_request;
 
       PF::Util::ByteVector<buffer_size> buffer;
       auto write_status = test_message.write(buffer, BE::message_descriptors);
-      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarms);
+      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::parameters_request);
       auto parse_status = test_message.parse(buffer, BE::message_descriptors);
 
       THEN("the final status should be ok") {
@@ -92,10 +100,10 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
 
     WHEN("data is parsed from an invalid buffer") {
       constexpr size_t buffer_size = 253UL;
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+      test_message.payload.tag = PF::Application::MessageTypes::alarm_limits;
       PF::Util::ByteVector<buffer_size> buffer;
       buffer.resize(buffer_size);
-      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarms);
+      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarm_limits);
       auto status = test_message.parse(buffer, BE::message_descriptors);
 
       THEN("the final status should be ok") {
@@ -105,7 +113,7 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
 
     WHEN("data is parsed with an invalid type") {
       constexpr size_t buffer_size = 253UL;
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+      test_message.payload.tag = PF::Application::MessageTypes::alarm_limits;
       PF::Util::ByteVector<buffer_size> buffer;
       auto write_status = test_message.write(buffer, BE::message_descriptors);
       buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::unknown);
@@ -116,14 +124,17 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       }
     }
 
-    WHEN("the regular alarm data is parsed from the buffer") {
+    WHEN("the regular parameters data is parsed from the buffer") {
       constexpr size_t buffer_size = 253UL;
       PF::Protocols::MessageSender<TestMessage, num_descriptors> sender{BE::message_descriptors};
 
-      Alarms alarms;
-      alarms.alarm_one = true;
-      test_message.payload.set(alarms);
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+      Parameters parameters;
+      parameters.fio2=60;
+      parameters.flow=40;
+      parameters.mode=VentilationMode_hfnc;
+      parameters.rr=20;
+      test_message.payload.set(parameters);
+      test_message.payload.tag= PF::Application::MessageTypes::parameters;
 
       PF::Util::ByteVector<buffer_size> buffer;
       auto transform_status = sender.transform(test_message, buffer);
@@ -137,10 +148,12 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       constexpr size_t buffer_size = 253UL;
       PF::Protocols::MessageSender<TestMessage, num_descriptors> sender{BE::message_descriptors};
 
-      Alarms alarms;
-      alarms.alarm_one = true;
-      test_message.payload.set(alarms);
-      test_message.payload.tag = PF::Application::MessageTypes::alarms;
+      SensorMeasurements sensor_measurments;
+      sensor_measurments.flow=30;
+      sensor_measurments.spo2=92;
+      sensor_measurments.fio2=50;
+      test_message.payload.set(sensor_measurments);
+      test_message.payload.tag= PF::Application::MessageTypes::sensor_measurements;
 
       PF::Util::ByteVector<buffer_size> buffer;
       auto transform_status = sender.transform(test_message, buffer);
@@ -265,18 +278,25 @@ SCENARIO("Protocols::Message behaves correctly", "[messages]") {
       }
     }
 
-    WHEN("ping data is written into the buffer") {
+    WHEN("alarm limits data is written into the buffer") {
       constexpr size_t buffer_size = 253UL;
       PF::Protocols::MessageReceiver<TestMessage, num_descriptors> receiver{BE::message_descriptors};
+      Range range1;
+      range1.lower=50;
+      range1.upper=70;
+
+      Range range2;
+      range2.lower=30;
+      range2.upper=60;
 
       PF::Util::ByteVector<buffer_size> buffer;
-      Ping ping;
-      ping.id = 256;
-      test_message.payload.set(ping);
-      test_message.payload.tag = PF::Application::MessageTypes::ping;
+      AlarmLimits alarm_limits;
+      alarm_limits.fio2=range1;
+      alarm_limits.flow=range2;
+      test_message.payload.set(alarm_limits);
 
       auto write_status = test_message.write(buffer, BE::message_descriptors);
-      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::ping);
+      buffer[TestMessage::type_offset] = static_cast<uint8_t>(PF::Application::MessageTypes::alarm_limits);
 
       auto transform_status = receiver.transform(buffer, test_message);
 
--- firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/States.cpp
+++ firmware/ventilator-controller-stm32/Core/Test/Pufferfish/Protocols/States.cpp
@@ -56,11 +56,12 @@ SCENARIO("Protocols::State behaves correctly", "[Datagram]") {
 
     WHEN("single state is written to it") {
       PF::Application::StateSegment input;
-      Alarms alarms;
-      alarms.time = 0x12345678;
-      alarms.alarm_one = true;
-      alarms.alarm_two = false;
-      input.set(alarms);
+      ParametersRequest parameters_request;
+      parameters_request.ventilating=true;
+      parameters_request.fio2=40;
+      parameters_request.mode=VentilationMode_hfnc;
+      parameters_request.flow=60;
+      input.tag=PF::Application::MessageTypes::parameters_request;
 
       BackendStateSynchronizer synchronizer{states, PF::Driver::Serial::Backend::state_sync_schedule};
 
@@ -69,11 +70,14 @@ SCENARIO("Protocols::State behaves correctly", "[Datagram]") {
       auto input_status = synchronizer.input(input);
 
       PF::Application::StateSegment output;
+      output.tag= PF::Application::MessageTypes::parameters_request;
       auto output_status1 = synchronizer.output(output);
 
       ctime += 10;
       auto input_ctime_status2 = synchronizer.input(ctime);
       auto output_status2 = synchronizer.output(output);
+
+      auto expected_output = states.parameters_request();
 
       THEN("the final status should be ok") {
         REQUIRE(input_ctime_status1 == BackendStateSynchronizer::InputStatus::ok);
