2d778903293eac8fbe42d125224953466f223c8a
rohan <purohit.rohan@gmail.com>
2021-01-29 03:03:53 +0530

    backend: update test descriptions

--- backend/tests/unit/protocols/test_server.py
+++ backend/tests/unit/protocols/test_server.py
@@ -1,48 +1,49 @@
 """Test the functionality of protocols.server classes."""
 
 from typing import Tuple
-import pytest as pt # type: ignore
+import pytest as pt  # type: ignore
 import hypothesis as hp
 import hypothesis.strategies as st
 import betterproto
 
-from ventserver.protocols import server,backend,file
+from ventserver.protocols import server, backend, file
 from ventserver.protocols.protobuf import mcu_pb as pb
 
 example_messages_good = [
-    (35.065809,pb.SensorMeasurements(paw=20)),
-    (58.719322,pb.CycleMeasurements(ve=300)),
-    (58.719322,pb.Parameters(fio2=80)),
-    (58.719322,pb.ParametersRequest(fio2=60)),
-    (50.268176,pb.Ping(id=256)),
-    (00.268176,pb.Announcement(announcement=b'hello!')),
+    (pb.SensorMeasurements(paw=20)),
+    (pb.CycleMeasurements(ve=300)),
+    (pb.Parameters(fio2=80)),
+    (pb.ParametersRequest(fio2=60)),
+    (pb.Ping(id=256)),
+    (pb.Announcement(announcement=b'hello!')),
 ]
 
 example_event = [
     (
-        43.000230,
+        0,
         b'U\xa3\x13\xae\x04\x08\xdd\xe1\xce\xce\xcd.\x10\x06\x1d\x00\x00' +
         b'\xc0@%\x00\x00\xf0A-\x00\x00HC5\x00\x00\xf0A=\x00\x00\x00@E\x00' +
         b'\x00pBM\x00\x00\x08B', (0, True), 'Parameters'
     ),
     (
-        35.065809,
+        40.5,
         b'\xacO\xb2\x8c\x03\x08\x89\x99\xcf\xce\xcd.\x15\x00\x00zC\x1d' +
         b'\x00\x00\xc8A%\x00\x00pA-\x00\x00 A5\x00\x00\xc0@=\x00\x00pA',
         (32, False), 'CycleMeasurements'
     ),
     (
-        58.719322,
+        23.2000022,
         b'\xff\r\xca<\x05\x08\x90\xf5\xce\xce\xcd.\x10\x06\x1d\x00\x00' +
         b'\xe0@%\x00\x00\xa0A-\x00\x00\x96C5\x00\x00\xa0B=\x00\x00@@E' +
-        b'\x00\x00HBM\x00\x00\xa0A',(29, True), 'ParametersRequest'
+        b'\x00\x00HBM\x00\x00\xa0A', (29, True), 'ParametersRequest'
     ),
     (
-        58.719322, b'/3Z\xf6\x01\x08\xe9\xc0\xe1\xce\xcd.\x10\x01',
+        123.22222,
+        b'/3Z\xf6\x01\x08\xe9\xc0\xe1\xce\xcd.\x10\x01',
         (55, True), 'Alarms'
     ),
     (
-        58.719322,
+        1222.222,
         b'%W\xb29\x02\x08\xe9\xc0\xe1\xce\xcd.\x10\n\x1d\x00\x00\xa0A%' +
         b'\x00\x00\xc8A-\x00\x00HB5\x00\x00HB=\x00\x00\xc2B',
         (45, True), 'SensorMeasurements'
@@ -50,28 +51,29 @@ example_event = [
 ]
 
 #  Server
+
+
 @hp.given(example_time=st.times())
-@hp.example(example_time='13:34:52.681276')
-@hp.example(example_time='13:52:52.681276')
-@hp.example(example_time='01:52:52.681276')
-@hp.example(example_time='16:37:41.745141')
-@hp.example(example_time='16:37:00.745141')
-@hp.example(example_time='16:37:00.002910')
+@hp.example(example_time='0')
 def test_frontend_connection_event(example_time: float) -> None:
     """
     Scenario: check if server connects to frontend
     """
     # Given: Frontendconnection event object
     # When: event is passed at some time
-    frontend_connection = server.FrontendConnectionEvent(example_time, True)
+    frontend_connection = server.FrontendConnectionEvent()
+    assert frontend_connection.has_data() is False
+    frontend_connection.last_connection_time = example_time
+    frontend_connection.is_frontend_connected = False
     # Then: frontend connection has data
     assert frontend_connection.has_data() is True
 
+
 @pt.mark.parametrize('example_time, data, rotary, state_type', example_event)
 def test_receive_event(
-    example_time:float, data: bytes,
-    rotary: Tuple[int, bool],
-    state_type: str) -> None:
+        example_time: float, data: bytes,
+        rotary: Tuple[int, bool],
+        state_type: str) -> None:
     """
     Scenario: check if receive event has data
     """
@@ -81,24 +83,38 @@ def test_receive_event(
         data=data,
         state_type=state_type
     )
-    receive_event = server.ReceiveEvent(
-        example_time, data, data, rotary, filedata)
-    print(receive_event)
+    receive_event = server.ReceiveEvent()
+    #  example_time, data, data, rotary, filedata
+    assert receive_event.has_data() is False
     # Then: receive event has data
+    receive_event.time = example_time
+    assert receive_event.has_data() is True
+    receive_event.serial_receive = data
+    assert receive_event.has_data() is True
+    receive_event.websocket_receive = data
+    assert receive_event.has_data() is True
+    receive_event.rotary_encoder_receive = rotary
+    assert receive_event.has_data() is True
+    receive_event.file_receive = filedata
     assert receive_event.has_data() is True
 
-@pt.mark.parametrize('_,payload', example_messages_good)
+
+@pt.mark.parametrize('payload', example_messages_good)
 def test_receive_output_event(
-    _: float, payload: betterproto.Message) -> None:
+        payload: betterproto.Message) -> None:
     """
     Scenario: check if receive output event has data
     """
     # Given: A backend output event object
     backend_output_event = backend.OutputEvent(payload)
     # When: backend output event is passed to ReceiveOutputEvent
-    server_output = server.ReceiveOutputEvent(backend_output_event, False)
+    server_output = server.ReceiveOutputEvent()
+    assert server_output.has_data() is False
+    server_output.server_send = backend_output_event
+    server_output.frontend_delayed = True
     # Then: server output has data
     assert server_output.has_data() is True
+
 
 @hp.given(data=st.binary(min_size=0x00000000, max_size=0xffffffffff))
 @hp.example(data=b'\x00\x00\x00\x00')
@@ -109,16 +125,16 @@ def test_send_output_event_has_data(data: bytes) -> None:
     """
     # Given: A SendOutputEvent object
     # When: data is written to it
-    send_output_event = server.SendOutputEvent(data,data)
+    send_output_event = server.SendOutputEvent(data, data)
     # Then: server output event has data
     assert send_output_event.has_data() is True
     assert send_output_event.websocket_send is not None
 
+
 @hp.given(data=st.binary(
     min_size=0x00000000, max_size=0xffffffffff),
     example_time=st.times())
-@hp.example(data=b'\x00\x00\x00\x00',example_time='13:34:52.681276')
-@hp.example(data=b'\xff\xff\xff\xff',example_time='13:52:52.681276')
+@hp.example(data=b'\x00\x00\x00\x00', example_time='0')
 # @hp.example(example_time='01:52:52.681276')
 def test_serial_receive(data: bytes, example_time: float) -> None:
     """
@@ -130,13 +146,15 @@ def test_serial_receive(data: bytes, example_time: float) -> None:
     # Then: receive event is not None
     assert serial_receive is not None
     assert isinstance(serial_receive, server.ReceiveEvent)
+    assert serial_receive.serial_receive == data
+    assert serial_receive.time == example_time
+
 
 @hp.given(data=st.binary(
     min_size=0x00000000, max_size=0xffffffffff),
     example_time=st.times())
-@hp.example(data=b'\x00\x00\x00\x00',example_time='13:34:52.681276')
-@hp.example(data=b'\x12\x23\x12\x45',example_time='13:34:52.681276')
-@hp.example(data=b'\xff\xff\xff\xff',example_time='13:52:52.681276')
+@hp.example(data=b'\x00\x00\x00\x00', example_time='13:34:52.681276')
+@hp.example(data=b'\xff\xff\xff\xff', example_time='0')
 # @hp.example(example_time='01:52:52.681276')
 def test_websocket_receive(data: bytes, example_time: float) -> None:
     """
@@ -148,13 +166,15 @@ def test_websocket_receive(data: bytes, example_time: float) -> None:
     # Then: receive event is not None
     assert websocket_receive is not None
     assert isinstance(websocket_receive, server.ReceiveEvent)
+    assert websocket_receive.websocket_receive == data
+    assert websocket_receive.time == example_time
 
-@hp.given(data=st.tuples(st.integers(), st.booleans()),
-    example_time=st.times())
-@hp.example(data=(1, True), example_time='13:34:52.681276')
+
+@hp.given(data=st.tuples(st.integers(min_value=0), st.booleans()),
+          example_time=st.times())
 def test_rotary_encoder_receive(
-    data: Tuple[int, bool],
-    example_time: float) -> None:
+        data: Tuple[int, bool],
+        example_time: float) -> None:
     """
     Scenario: rotary encoder receive behaves properly
     """
@@ -168,11 +188,12 @@ def test_rotary_encoder_receive(
 
 # Filters
 
-@pt.mark.parametrize('example_time, data, rotary, state_type', example_event)
+
+@pt.mark.parametrize('example_time,data, rotary, state_type', example_event)
 def test_receive_filter_output(
-    example_time:float, data: bytes,
-    rotary: Tuple[int, bool],
-    state_type: str) -> None:
+        example_time: float, data: bytes,
+        rotary: Tuple[int, bool],
+        state_type: str) -> None:
     """
     Scenario: Receive Filter behaves properly
     """
@@ -181,8 +202,12 @@ def test_receive_filter_output(
         data=data,
         state_type=state_type
     )
-    receive_event = server.ReceiveEvent(
-        example_time, data, data, rotary, filedata)
+    receive_event = server.ReceiveEvent()
+    receive_event.time = example_time
+    receive_event.serial_receive = data
+    receive_event.websocket_receive = data
+    receive_event.rotary_encoder_receive = rotary
+    receive_event.file_receive = filedata
     # When: No event is passed to the filter
     recieve_filter = server.ReceiveFilter()
     # When: event is passed to the filter
@@ -192,10 +217,12 @@ def test_receive_filter_output(
     # Then: output is of type ReceiveOutputvent
     assert output is not None
     assert isinstance(output, server.ReceiveOutputEvent)
+    # assert output.has_data() is True
 
-@pt.mark.parametrize('_,payload', example_messages_good)
+
+@pt.mark.parametrize('payload', example_messages_good)
 def test_send_filter(
-    _: float, payload: betterproto.Message) -> None:
+        payload: betterproto.Message) -> None:
     """
     Scenario: Send Filter behaves properly
     """
@@ -208,5 +235,4 @@ def test_send_filter(
     # Then: output is of type SendOutputEvent
     output = server_send_filter.output()
 
-    assert output is not None
     assert isinstance(output, server.SendOutputEvent)
--- backend/tests/unit/simulation/test_alarms.py
+++ backend/tests/unit/simulation/test_alarms.py
@@ -3,7 +3,7 @@
 import typing
 from typing import Mapping, Optional, Type
 # from math import isclose
-import pytest as pt # type: ignore
+import pytest as pt  # type: ignore
 # from pytest import approx
 import hypothesis as hp
 import hypothesis.strategies as st
@@ -14,51 +14,51 @@ from ventserver.protocols.application import lists
 from ventserver.protocols.protobuf import mcu_pb as pb
 
 example_hfnc: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]] = {
-        pb.Parameters: pb.Parameters(ventilating=True, mode=pb.VentilationMode.hfnc),
-        pb.SensorMeasurements: pb.SensorMeasurements(spo2=90, fio2=20),
-        pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=92, upper=100),
-        fio2=pb.Range(lower=21, upper=100)),
-        pb.ActiveLogEvents: pb.ActiveLogEvents()
+                      Optional[betterproto.Message]] = {
+    pb.Parameters: pb.Parameters(ventilating=True, mode=pb.VentilationMode.hfnc),
+    pb.SensorMeasurements: pb.SensorMeasurements(spo2=90, fio2=20),
+    pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=92, upper=100),
+                                   fio2=pb.Range(lower=21, upper=100)),
+    pb.ActiveLogEvents: pb.ActiveLogEvents()
 }
 
 example_hfnc_bad: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]] = {
-        pb.Parameters: pb.Parameters(rr=30, mode=pb.VentilationMode.hfnc),
-        pb.SensorMeasurements: pb.SensorMeasurements(spo2=90, fio2=20),
-        pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=92, upper=100),
-        fio2=pb.Range(lower=21, upper=100)),
-        pb.ActiveLogEvents: pb.ActiveLogEvents(id=0)
+                          Optional[betterproto.Message]] = {
+    pb.Parameters: pb.Parameters(rr=30, mode=pb.VentilationMode.hfnc),
+    pb.SensorMeasurements: pb.SensorMeasurements(spo2=90, fio2=20),
+    pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=92, upper=100),
+                                   fio2=pb.Range(lower=21, upper=100)),
+    pb.ActiveLogEvents: pb.ActiveLogEvents(id=0)
 }
 
 example_pcac_min: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]] = {
-        pb.Parameters: pb.Parameters(mode=pb.VentilationMode.pc_ac,
-        ventilating=True),
-        pb.SensorMeasurements: pb.SensorMeasurements(spo2=30, fio2=10),
-        pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=50, upper=100),
-        fio2=pb.Range(lower=21, upper=100)),
-        pb.ActiveLogEvents: pb.ActiveLogEvents()
+                          Optional[betterproto.Message]] = {
+    pb.Parameters: pb.Parameters(mode=pb.VentilationMode.pc_ac,
+                                 ventilating=True),
+    pb.SensorMeasurements: pb.SensorMeasurements(spo2=30, fio2=10),
+    pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=50, upper=100),
+                                   fio2=pb.Range(lower=21, upper=100)),
+    pb.ActiveLogEvents: pb.ActiveLogEvents()
 }
 
 example_pcac_max: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]] = {
-        pb.Parameters: pb.Parameters(mode=pb.VentilationMode.pc_ac,
-        ventilating=True),
-        pb.SensorMeasurements: pb.SensorMeasurements(spo2=102, fio2=102),
-        pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=95, upper=100),
-        fio2=pb.Range(lower=21, upper=100)),
-        pb.ActiveLogEvents: pb.ActiveLogEvents()
+                          Optional[betterproto.Message]] = {
+    pb.Parameters: pb.Parameters(mode=pb.VentilationMode.pc_ac,
+                                 ventilating=True),
+    pb.SensorMeasurements: pb.SensorMeasurements(spo2=102, fio2=102),
+    pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=95, upper=100),
+                                   fio2=pb.Range(lower=21, upper=100)),
+    pb.ActiveLogEvents: pb.ActiveLogEvents()
 }
 
 example_bad: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]] = {
-        pb.Parameters: pb.Parameters(rr=30, mode=pb.VentilationMode.vc_ac),
-        pb.SensorMeasurements: pb.SensorMeasurements(time=10,
-        fio2=30, flow=50, paw=20),
-        pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=0, upper=100),
-        fio2=pb.Range(lower=21, upper=100)),
-        pb.ActiveLogEvents: pb.ActiveLogEvents(id=0)
+                     Optional[betterproto.Message]] = {
+    pb.Parameters: pb.Parameters(rr=30, mode=pb.VentilationMode.vc_ac),
+    pb.SensorMeasurements: pb.SensorMeasurements(time=10,
+                                                 fio2=30, flow=50, paw=20),
+    pb.AlarmLimits: pb.AlarmLimits(spo2=pb.Range(lower=0, upper=100),
+                                   fio2=pb.Range(lower=21, upper=100)),
+    pb.ActiveLogEvents: pb.ActiveLogEvents(id=0)
 }
 
 state_hfnc_bad = [example_hfnc_bad]
@@ -74,12 +74,126 @@ synchronizer = lists.SendSynchronizer(
     max_len=10, max_segment_len=5
 )
 
-@hp.given(example_time=st.floats(min_value=0, max_value=5, allow_nan=False))
+
+@pt.mark.parametrize('state', state_bad)
+def test_pcac_service_invalid_mode(
+    state: Mapping[Type[betterproto.Message],
+                   Optional[betterproto.Message]],
+) -> None:
+    '''
+    Scenario: The Service class correctly handles alarms
+    '''
+    # Given: A PCAC Service class object is constructed with default parameters
+    test_service = alarms.Service()
+    # When: The ventilation mode in parameters class is not equal to either pcac or hfnc
+    parameters = typing.cast(
+        pb.Parameters, state[pb.Parameters])
+    alarm_limits = typing.cast(
+        pb.AlarmLimits, state[pb.AlarmLimits])
+    sensor_measurments = typing.cast(
+        pb.SensorMeasurements, state[pb.SensorMeasurements])
+    active_log_events = typing.cast(
+        pb.ActiveLogEvents, state[pb.ActiveLogEvents])
+
+    test_service.transform(parameters, alarm_limits,
+                           sensor_measurments, active_log_events, synchronizer)
+    # Then: The alarms are not updated for any parameters
+    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode) is None
+
+
+@pt.mark.parametrize('state', state_hfnc_bad)
+def test_pcac_service_invalid_ventilating(
+    state: Mapping[Type[betterproto.Message],
+                   Optional[betterproto.Message]],
+) -> None:
+    '''
+    # Scenario: The Service class correctly handles alarms
+    '''
+    # Given: A PCAC Service class object is constructed with default parameters
+    test_service = alarms.Service()
+    # When: The ventilating value in parameter class is false
+    parameters = typing.cast(
+        pb.Parameters, state[pb.Parameters])
+    alarm_limits = typing.cast(
+        pb.AlarmLimits, state[pb.AlarmLimits])
+    sensor_measurments = typing.cast(
+        pb.SensorMeasurements, state[pb.SensorMeasurements])
+    active_log_events = typing.cast(
+        pb.ActiveLogEvents, state[pb.ActiveLogEvents])
+
+    test_service.transform(parameters, alarm_limits,
+                           sensor_measurments, active_log_events, synchronizer)
+    # Then: All Active alarms are deactivated
+    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode) is None
+
+
+@pt.mark.parametrize('state', state_pcac_min)
+def test_pcac_service_alarm_min(
+    state: Mapping[Type[betterproto.Message],
+                   Optional[betterproto.Message]],
+) -> None:
+    '''
+    # Scenario: The Service class correctly handles pcac alarms for SpO2
+    '''
+    # Given: a pc_ac alarm service object
+    test_service = alarms.Service()
+    # When: The sensor measurments value for Spo2 & fio2 is lesser than the minimum alarm_limit for spo2 & fio2
+    parameters = typing.cast(
+        pb.Parameters, state[pb.Parameters])
+    alarm_limits = typing.cast(
+        pb.AlarmLimits, state[pb.AlarmLimits])
+    sensor_measurments = typing.cast(
+        pb.SensorMeasurements, state[pb.SensorMeasurements])
+    active_log_events = typing.cast(
+        pb.ActiveLogEvents, state[pb.ActiveLogEvents])
+
+    test_service.transform(parameters, alarm_limits,
+                           sensor_measurments, active_log_events, synchronizer)
+
+    # Then: The alarm for spo2 & fio2 too low is triggered
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.fio2_too_low) is not None
+
+
+@pt.mark.parametrize('state', state_pcac_max)
+def test_pcac_alarm_max(
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
+    '''
+    # Scenario: The Service class correctly handles pcac alarms for SpO2
+    '''
+    # Given: a pc_ac alarm service object
+    test_service = alarms.Service()
+    # When: The sensor measurments value for Spo2 & fio2 is greater than the maximum alarm_limit for spo2 & fio2
+    parameters = typing.cast(
+        pb.Parameters, state[pb.Parameters])
+    alarm_limits = typing.cast(
+        pb.AlarmLimits, state[pb.AlarmLimits])
+    sensor_measurments = typing.cast(
+        pb.SensorMeasurements, state[pb.SensorMeasurements])
+    active_log_events = typing.cast(
+        pb.ActiveLogEvents, state[pb.ActiveLogEvents])
+
+    test_service.transform(parameters, alarm_limits,
+                           sensor_measurments, active_log_events, synchronizer)
+    # Then: The alarm for spo2 & fio2 too high is triggered
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.fio2_too_high) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_high) is not None
+
+
+@hp.given(example_time=st.floats(min_value=0))
 @pt.mark.parametrize('state', state_bad)
 def test_invalid_mode(
-    state: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]],
-    example_time: float) -> None:
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
     '''
     Scenario: alarm services behaving properly
     '''
@@ -91,12 +205,13 @@ def test_invalid_mode(
     # Then: alarms are not updated for parameters
     assert test_service._manager.active_alarm_ids.get(pb.LogEventCode) is None
 
-@hp.given(example_time=st.floats(min_value=0, max_value=5, allow_nan=False))
+
+@hp.given(example_time=st.floats(min_value=0))
 @pt.mark.parametrize('state', state_hfnc_bad)
 def test_invalid_ventilating(
-    state: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]],
-    example_time: float) -> None:
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
     '''
     Scenario: alarm services behaving properly
     '''
@@ -104,16 +219,18 @@ def test_invalid_ventilating(
     test_service = alarms.Services()
     # When: parameter ventilating mode is false
     test_service.transform(example_time, state, synchronizer)
-    
-    # Then: alarms are not updated for parameters
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.spo2_too_low) is None
 
-@hp.given(example_time=st.floats(min_value=0, max_value=5, allow_nan=False))
+    # Then: alarms are not updated for parameters
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is None
+
+
+@hp.given(example_time=st.floats(min_value=0))
 @pt.mark.parametrize('state', state_pcac_min)
 def test_pcac_alarm_min(
-    state: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]],
-    example_time: float) -> None:
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
     '''
     Scenario: alarm services behaving properly
     '''
@@ -123,15 +240,18 @@ def test_pcac_alarm_min(
     test_service.transform(example_time, state, synchronizer)
 
     # Then: alarm for spo2 & fio2 too low is triggered
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.spo2_too_low) is not None
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.fio2_too_low) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.fio2_too_low) is not None
 
-@hp.given(example_time=st.floats(min_value=0, max_value=5, allow_nan=False))
+
+@hp.given(example_time=st.floats(min_value=0))
 @pt.mark.parametrize('state', state_pcac_max)
 def test_pcac_alarm_max(
-    state: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]],
-    example_time: float) -> None:
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
     '''
     Scenario: alarm services behaving properly
     '''
@@ -140,16 +260,20 @@ def test_pcac_alarm_max(
     # When: sensor measurments fio2 is greater than upper alarm_limit for fio2
     test_service.transform(example_time, state, synchronizer)
     # Then: alarm for spo2 & fio2 too high is triggered
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.spo2_too_low) is None
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.fio2_too_high) is not None
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.spo2_too_high) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.fio2_too_high) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_high) is not None
 
-@hp.given(example_time=st.floats(min_value=0, max_value=5, allow_nan=False))
+
+@hp.given(example_time=st.floats(min_value=0))
 @pt.mark.parametrize('state', state_hfnc)
 def test_hfnc_alarm_min(
-    state: Mapping[Type[betterproto.Message],
-    Optional[betterproto.Message]],
-    example_time: float) -> None:
+        state: Mapping[Type[betterproto.Message],
+                       Optional[betterproto.Message]],
+        example_time: float) -> None:
     '''
     Scenario: alarm services behaving properly
     '''
@@ -159,6 +283,7 @@ def test_hfnc_alarm_min(
     test_service.transform(example_time, state, synchronizer)
 
     # Then: alarm for spo2 & fio2 too low is triggered
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.spo2_too_low) is not None
-    assert test_service._manager.active_alarm_ids.get(pb.LogEventCode.fio2_too_low) is not None
-
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.spo2_too_low) is not None
+    assert test_service._manager.active_alarm_ids.get(
+        pb.LogEventCode.fio2_too_low) is not None
