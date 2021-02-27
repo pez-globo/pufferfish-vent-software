42591183b2f866668f21359af1fa7744ef12f802
rohan <purohit.rohan@gmail.com>
2021-02-27 09:09:54 +0530

    firmware: Update helper functions

--- firmware/ventilator-controller-stm32/Core/Test/Inc/Pufferfish/Test/Util.h
+++ firmware/ventilator-controller-stm32/Core/Test/Inc/Pufferfish/Test/Util.h
@@ -4,7 +4,7 @@
  *  Created on: Nov 17, 2020
  *      Author: Renji Panicker
  *
- *  A set of helper functins for writing and debugging test cases
+ *  A set of helper functions for writing and debugging test cases
  */
 
 #pragma once
@@ -15,86 +15,36 @@
 
 namespace Pufferfish::Util {
 
+// operator to match ByteVector and HexString
 template <size_t payload_size>
-inline bool operator==(
-    const Pufferfish::Util::ByteVector<payload_size>& lhs, const std::string& rhs) {
-  if (lhs.size() != rhs.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < lhs.size(); ++i) {
-    if (static_cast<uint8_t>(lhs[i]) != static_cast<uint8_t>(rhs.at(i))) {
-      return false;
-    }
-  }
-  return true;
-}
+bool operator==(const Pufferfish::Util::ByteVector<payload_size>& lhs, const std::string& rhs);
 
+// operator to match ByteVector and HexString
 template <size_t payload_size>
-inline bool operator!=(
-    const Pufferfish::Util::ByteVector<payload_size>& lhs, const std::string& rhs) {
-  return !(lhs == rhs);
-}
+bool operator!=(const Pufferfish::Util::ByteVector<payload_size>& lhs, const std::string& rhs);
 
+// operator to match two ByteVector's
 template <size_t payload_size>
-inline bool operator==(
+bool operator==(
     const Pufferfish::Util::ByteVector<payload_size>& lhs,
-    const Pufferfish::Util::ByteVector<payload_size>& rhs) {
-  if (lhs.size() != rhs.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < lhs.size(); ++i) {
-    if (static_cast<uint8_t>(lhs[i]) != static_cast<uint8_t>(rhs[i])) {
-      return false;
-    }
-  }
-  return true;
-}
+    const Pufferfish::Util::ByteVector<payload_size>& rhs);
 
+// operator to match two ByteVector's
 template <size_t payload_size>
-inline bool operator!=(
+bool operator!=(
     const Pufferfish::Util::ByteVector<payload_size>& lhs,
-    const Pufferfish::Util::ByteVector<payload_size>& rhs) {
-  return !(lhs == rhs);
-}
+    const Pufferfish::Util::ByteVector<payload_size>& rhs);
 
+// converts HexString to ByteVector
 template <size_t payload_size>
-inline bool convert_string_to_byte_vector(
-    const std::string& input_string, Pufferfish::Util::ByteVector<payload_size>& output_buffer) {
-  if (input_string.size() >= payload_size) {
-    return false;
-  }
-  for (const auto& ch : input_string) {
-    output_buffer.push_back(ch);
-  }
-  return true;
-}
+bool convert_string_to_byte_vector(
+    const std::string& input_string, Pufferfish::Util::ByteVector<payload_size>& output_buffer);
 
+// converts ByteVector to HexString
 template <size_t payload_size>
-inline std::string convert_byte_vector_to_hex_string(
-    const Pufferfish::Util::ByteVector<payload_size>& input_buffer) {
-  std::string output_string;
-  for (size_t i = 0; i < input_buffer.size(); ++i) {
-    auto& ch = input_buffer[i];
-    output_string += "\\x";
-    int c1 = ((ch & 0xf0) >> 4);
-    if ((c1 >= 0) && (c1 <= 9)) {
-      output_string += std::to_string(c1);
-    } else if ((c1 >= 10) && (c1 <= 15)) {
-      output_string += static_cast<char>(c1 - 10 + 'A');
-    } else {
-      output_string += ('?');
-    }
-
-    c1 = (ch & 0x0f);
-    if ((c1 >= 0) && (c1 <= 9)) {
-      output_string += std::to_string(c1);
-    } else if ((c1 >= 10) && (c1 <= 15)) {
-      output_string += static_cast<char>(c1 - 10 + 'A');
-    } else {
-      output_string += ('?');
-    }
-  }
-  return output_string;
-}
+std::string convert_byte_vector_to_hex_string(
+    const Pufferfish::Util::ByteVector<payload_size>& input_buffer);
 
 }  // namespace Pufferfish::Util
+
+#include "Util.tpp"
